3b4e165830a39be7db0fc9cdc51b51fb7dcfc37f
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2016-12-29 20:14:27 +0100

    Improved argument order for generic syscall functions so that less register reordering is required.

--- include/syscalls.h
+++ include/syscalls.h
@@ -7,12 +7,12 @@
 #include <stdint.h>
 
 uintptr_t linux_syscall0(uintptr_t num);
-uintptr_t linux_syscall1(uintptr_t num, uintptr_t arg1);
-uintptr_t linux_syscall2(uintptr_t num, uintptr_t arg1, uintptr_t arg2);
-uintptr_t linux_syscall3(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3);
-uintptr_t linux_syscall4(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4);
-uintptr_t linux_syscall5(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4, uintptr_t arg5);
-uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4, uintptr_t arg5, uintptr_t arg6);
+uintptr_t linux_syscall1(uintptr_t arg1, uintptr_t num);
+uintptr_t linux_syscall2(uintptr_t arg1, uintptr_t arg2, uintptr_t num);
+uintptr_t linux_syscall3(uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t num);
+uintptr_t linux_syscall4(uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4, uintptr_t num);
+uintptr_t linux_syscall5(uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4, uintptr_t arg5, uintptr_t num);
+uintptr_t linux_syscall6(uintptr_t arg1, uintptr_t arg2, uintptr_t arg3, uintptr_t arg4, uintptr_t arg5, uintptr_t arg6, uintptr_t num);
 
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL0_RET(name, ret_t)                           \
 	static inline enum linux_error_t linux_ ## name(ret_t* const result)       \
@@ -27,7 +27,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL1_RET(name, arg1_t, arg1, ret_t)                              \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, ret_t* const result)     \
 	{                                                                                           \
-		uintptr_t const ret = linux_syscall1(linux_syscall_name_ ## name, (uintptr_t)arg1); \
+		uintptr_t const ret = linux_syscall1((uintptr_t)arg1, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                              \
 			return (enum linux_error_t)-ret;                                            \
 		if (result)                                                                         \
@@ -37,7 +37,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL2_RET(name, arg1_t, arg1, arg2_t, arg2, ret_t)                                 \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, ret_t* const result)   \
 	{                                                                                                            \
-		uintptr_t const ret = linux_syscall2(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2); \
+		uintptr_t const ret = linux_syscall2((uintptr_t)arg1, (uintptr_t)arg2, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                               \
 			return (enum linux_error_t)-ret;                                                             \
 		if (result)                                                                                          \
@@ -47,7 +47,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL3_RET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, ret_t)                                    \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, ret_t* const result) \
 	{                                                                                                                             \
-		uintptr_t const ret = linux_syscall3(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3); \
+		uintptr_t const ret = linux_syscall3((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                                                \
 			return (enum linux_error_t)-ret;                                                                              \
 		if (result)                                                                                                           \
@@ -57,7 +57,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL4_RET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4, ret_t)                                         \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4, ret_t* const result) \
 	{                                                                                                                                                \
-		uintptr_t const ret = linux_syscall4(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4);   \
+		uintptr_t const ret = linux_syscall4((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, linux_syscall_name_ ## name);   \
 		if (linux_syscall_returned_error(ret))                                                                                                   \
 			return (enum linux_error_t)-ret;                                                                                                 \
 		if (result)                                                                                                                              \
@@ -67,7 +67,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL5_RET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4, arg5_t, arg5, ret_t)                                              \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4, arg5_t const arg5, ret_t* const result) \
 	{                                                                                                                                                                   \
-		uintptr_t const ret = linux_syscall5(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5);     \
+		uintptr_t const ret = linux_syscall5((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, linux_syscall_name_ ## name);     \
 		if (linux_syscall_returned_error(ret))                                                                                                                      \
 			return (enum linux_error_t)-ret;                                                                                                                    \
 		if (result)                                                                                                                                                 \
@@ -77,7 +77,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL6_RET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4, arg5_t, arg5, arg6_t, arg6, ret_t)                                                   \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4, arg5_t const arg5, arg6_t const arg6, ret_t* const result) \
 	{                                                                                                                                                                                      \
-		uintptr_t const ret = linux_syscall6(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, (uintptr_t)arg6);       \
+		uintptr_t const ret = linux_syscall6((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, (uintptr_t)arg6, linux_syscall_name_ ## name);       \
 		if (linux_syscall_returned_error(ret))                                                                                                                                         \
 			return (enum linux_error_t)-ret;                                                                                                                                       \
 		if (result)                                                                                                                                                                    \
@@ -96,7 +96,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL1_NORET(name, arg1_t, arg1)                                   \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1)                          \
 	{                                                                                           \
-		uintptr_t const ret = linux_syscall1(linux_syscall_name_ ## name, (uintptr_t)arg1); \
+		uintptr_t const ret = linux_syscall1((uintptr_t)arg1, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                              \
 			return (enum linux_error_t)-ret;                                            \
 		return linux_error_none;                                                            \
@@ -104,7 +104,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL2_NORET(name, arg1_t, arg1, arg2_t, arg2)                                      \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2)                        \
 	{                                                                                                            \
-		uintptr_t const ret = linux_syscall2(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2); \
+		uintptr_t const ret = linux_syscall2((uintptr_t)arg1, (uintptr_t)arg2, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                               \
 			return (enum linux_error_t)-ret;                                                             \
 		return linux_error_none;                                                                             \
@@ -112,7 +112,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL3_NORET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3)                                         \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3)                      \
 	{                                                                                                                             \
-		uintptr_t const ret = linux_syscall3(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3); \
+		uintptr_t const ret = linux_syscall3((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                                                \
 			return (enum linux_error_t)-ret;                                                                              \
 		return linux_error_none;                                                                                              \
@@ -120,7 +120,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL4_NORET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4)                                            \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4)                    \
 	{                                                                                                                                              \
-		uintptr_t const ret = linux_syscall4(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4); \
+		uintptr_t const ret = linux_syscall4((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                                                                 \
 			return (enum linux_error_t)-ret;                                                                                               \
 		return linux_error_none;                                                                                                               \
@@ -128,7 +128,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL5_NORET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4, arg5_t, arg5)                                               \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4, arg5_t const arg5)                  \
 	{                                                                                                                                                               \
-		uintptr_t const ret = linux_syscall5(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5); \
+		uintptr_t const ret = linux_syscall5((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                                                                                  \
 			return (enum linux_error_t)-ret;                                                                                                                \
 		return linux_error_none;                                                                                                                                \
@@ -136,7 +136,7 @@ uintptr_t linux_syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_
 #define LINUX_DETAIL_IMPLEMENT_SYSCALL6_NORET(name, arg1_t, arg1, arg2_t, arg2, arg3_t, arg3, arg4_t, arg4, arg5_t, arg5, arg6_t, arg6)                                                  \
 	static inline enum linux_error_t linux_ ## name(arg1_t const arg1, arg2_t const arg2, arg3_t const arg3, arg4_t const arg4, arg5_t const arg5, arg6_t const arg6)                \
 	{                                                                                                                                                                                \
-		uintptr_t const ret = linux_syscall6(linux_syscall_name_ ## name, (uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, (uintptr_t)arg6); \
+		uintptr_t const ret = linux_syscall6((uintptr_t)arg1, (uintptr_t)arg2, (uintptr_t)arg3, (uintptr_t)arg4, (uintptr_t)arg5, (uintptr_t)arg6, linux_syscall_name_ ## name); \
 		if (linux_syscall_returned_error(ret))                                                                                                                                   \
 			return (enum linux_error_t)-ret;                                                                                                                                 \
 		return linux_error_none;                                                                                                                                                 \
--- src/syscalls.asm
+++ src/syscalls.asm
@@ -7,60 +7,60 @@ GLOBAL linux_syscall4
 GLOBAL linux_syscall5
 GLOBAL linux_syscall6
 
-; TODO: improve argument order for maximum efficiency
+; C:
+; return value: rax
+; parameter: rdi, rsi, rdx, rcx, r8, r9, rest on stack in reversed order
+; stack alignment: 16
+; preserved: rbx, rsp, rbp, r12, r13, r14, r15
+; scratch: rax, rdi, rsi, rdx, rcx, r8, r9, r10, r11
+
+; Linux:
+; syscall number: rax
+; return value: rax
+; parameter: rdi, rsi, rdx, r10, r8, r9
+
+; ------------------------------------------------------------------------------
+; Generic functions
 
 linux_syscall0:
-	mov rax, rdi
+	mov rax, rdi ; arg1 -> #
 	syscall
 	ret
 
 linux_syscall1:
-	mov rax, rdi
-	mov rdi, rsi
+	mov rax, rsi ; arg2 -> #
 	syscall
 	ret
 
 linux_syscall2:
-	mov rax, rdi
-	mov rdi, rsi
-	mov rsi, rdx
+	mov rax, rdx ; arg3 -> #
 	syscall
 	ret
 
 linux_syscall3:
-	mov rax, rdi
-	mov rdi, rsi
-	mov rsi, rdx
-	mov rdx, rcx
+	mov rax, rcx ; arg4 -> #
 	syscall
 	ret
 
 linux_syscall4:
-	mov rax, rdi
-	mov rdi, rsi
-	mov rsi, rdx
-	mov rdx, rcx
-	mov r10, r8
+	mov r10, rcx ; arg4 -> arg4
+	mov rax,  r8 ; arg5 -> #
 	syscall
 	ret
 
 linux_syscall5:
-	mov rax, rdi
-	mov rdi, rsi
-	mov rsi, rdx
-	mov rdx, rcx
-	mov r10, r8
-	mov r8, r9
+	mov r10, rcx ; arg4 -> arg4
+	mov rax,  r9 ; arg6 -> #
 	syscall
 	ret
 
 linux_syscall6:
-	mov rax, rdi
-	mov rdi, rsi
-	mov rsi, rdx
-	mov rdx, rcx
-	mov r10, r8
-	mov r8, r9
-	mov r9, [rsp+8]
+	mov r10, rcx ; arg4 -> arg4
+	mov rax, [rsp+8] ; arg7 -> #
 	syscall
 	ret
+
+; Generic functions
+; ------------------------------------------------------------------------------
+
+; TODO: Add an own symbol for every syscall.
