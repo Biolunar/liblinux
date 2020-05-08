b840794628b7015660dcf19cf4cb3a484b8d886c
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2020-05-08 13:47:24 +0200

    Added hello world example.

---
+++ examples/hello-world/main.c
@@ -0,0 +1,14 @@
+#include <liblinux/start.h>
+#include <liblinux/linux.h>
+
+noreturn void linux_start(uintptr_t argc, char* argv[], char* envp[])
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+
+	char msg[] = "Hello, world!\n";
+	linux_write(1, msg, sizeof msg, 0);
+
+	linux_exit_group(0);
+}
