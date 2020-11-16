ef2d0fc012de094d5b850fb5626f0824ae645ca1
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2020-11-16 19:58:26 +0100

    Added const modifier in hello world example.

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -7,7 +7,7 @@ noreturn void linux_start(uintptr_t argc, char* argv[], char* envp[])
 	(void)argv;
 	(void)envp;
 
-	char msg[] = "Hello, world!\n";
+	char const msg[] = "Hello, world!\n";
 	linux_write(1, msg, sizeof msg, 0);
 
 	linux_exit_group(0);
