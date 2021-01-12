5ceb4ba501e870fe5984604dafe9c60eb358fe18
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2021-01-12 01:36:15 +0100

    Updated copyright date to 2021.

--- include/liblinux/constants.h
+++ include/liblinux/constants.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Mahdi Khanalizadeh
+ * Copyright 2018-2021 Mahdi Khanalizadeh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
